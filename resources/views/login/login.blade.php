1e7a383cf5c76dd5791c0a8f5b6f0410922c696e
dev <dev@MMs-iMac.local>
2017-03-07 16:41:26 -0300

    Finalizando tela de login.

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -46,11 +46,15 @@
                     <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
                     <span id="reauth-email" class="reauth-email"></span>
 
-                    <input type="email" id="inputEmail" onkeypress="limpaEmailLogin()" class="form-control" placeholder="Usuário" required autofocus>
-                    <i class="alertaFormLoginEmail">Este campo é obrigatório!</i>
+                    <div class="senha">
+                        <input type="email" id="inputEmail" onkeypress="limpaEmailLogin()" class="form-control" placeholder="Usuário" required autofocus>
+                        <i class="alertaFormLoginEmail">Este campo é obrigatório!</i>
+                    </div>
 
-                    <input type="password" id="inputPassword" onkeypress="limpaPasswordLogin()" class="form-control" placeholder="Senha" required>
-                    <i class="alertaFormLoginSenha">Este campo é obrigatório!</i>
+                    <div class="password">
+                        <input type="password" id="inputPassword" onkeypress="limpaPasswordLogin()" class="form-control" placeholder="Senha" required>
+                        <i class="alertaFormLoginSenha">Este campo é obrigatório!</i>
+                    </div>
 
                     <div id="remember" class="checkbox">
                         <label>
@@ -62,37 +66,5 @@
                 </form><!-- Fim-->
             </div>
         </div>
-
-
-
-    {{--<div class="container">--}}
-            {{--<div class="row" style="position: relative; padding-top: 10%; left: 35%;">--}}
-                {{--<div class="col-sm-12" style="width: 400px;">--}}
-                    {{--<div id="alertLogin"></div>--}}
-                    {{--<form class="form-group" id="formLogin">--}}
-                        {{--<input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="formLogin">--}}
-                                {{--<label for="email" class="col-sm-12">E-mail: </label>--}}
-                                {{--<input class="form-control col-sm-6" onkeypress="limpaEmailLogin()" type="email" name="email" id="email" />--}}
-                                {{--<i class="alertaFormLoginEmail">Este campo é obrigatório!</i>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="formLogin">--}}
-                                {{--<label for="senha" class="col-sm-12">Senha: </label>--}}
-                                {{--<input class="form-control col-sm-6" onkeypress="limpaSenhaLogin()" type="password" name="senha" id="senha" />--}}
-                                {{--<i class="alertaFormLoginSenha">Este campo é obrigatório!</i>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="row">--}}
-                            {{--<div style="padding-top: 20px">--}}
-                                {{--<input type="button" onclick="return submitLogin()" style="width: 100px" class="btn btn-info form-control" value="Entrar" />--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</form>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
     </body>
 </html>
